f699adad6edc65f9cb62217a907d949daebdac96
David Dellsperger <david.dellsperger@gmail.com>
2022-12-03 23:13:46 -0600

    Day 4 solution (#4)

---
+++ src/test/java/net/ddellspe/day04/Day04Test.java
@@ -0,0 +1,27 @@
+package net.ddellspe.day04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Day04Test {
+  @Test
+  public void providedInputTestPart1() {
+    assertEquals(2L, Day04.part1("example.txt"));
+  }
+
+  @Test
+  public void solutionPart1() {
+    System.out.println("Day 04 Part 1 Answer is: " + Day04.part1("input.txt"));
+  }
+
+  @Test
+  public void providedInputTestPart2() {
+    assertEquals(4L, Day04.part2("example.txt"));
+  }
+
+  @Test
+  public void solutionPart2() {
+    System.out.println("Day 04 Part 2 Answer is: " + Day04.part2("input.txt"));
+  }
+}
